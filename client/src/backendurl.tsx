ac3a267a3834f27cba29f942b734b9c5c9993641
SrinjayMitra <srinjaymitra918@gmail.com>
2025-01-07 00:32:11 -0800

    almost done fixed backend to any

--- client/src/backendurl.tsx
+++ client/src/backendurl.tsx
@@ -1,5 +1,5 @@
 // For production:
-export let URL:any = "https://brainly-alpha.vercel.app/api/v1";
+export let URL = "https://brainly-alpha.vercel.app/api/v1";
 
 // For local development:
 //  export let URL = "http://localhost:3000/api/v1";
