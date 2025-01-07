1b0358a606a6182a930a2f271e438df3cebd42f7
SrinjayMitra <srinjaymitra918@gmail.com>
2025-01-07 00:43:19 -0800

    stashing

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,10 +1,3 @@
-// export const SECRET_KEY = "SEXXXXXXXXXXXXX";
-// export const uri = "mongodb+srv://srinjay:Srinjay%402004@cluster0.1shl9.mongodb.net/brainly";
-// export const TINY_API_KEY: string = 'cnCKgifMJ3y3KHycpstvL7YMGxs9QYzPmC06gq0CZoQZBfwb8APULGuGKCVI';
-// export const apiUrl = `https://api.tinyurl.com/create?api_token=${TINY_API_KEY}`;
-
-
-
 export let URL = "https://brainly-alpha.vercel.app/api/v1";
 
 //  local
