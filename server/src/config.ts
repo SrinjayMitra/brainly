2b5047a3ccc12a42b122e688c2019c1b86ed6c2b
SrinjayMitra <srinjaymitra918@gmail.com>
2025-01-07 00:45:50 -0800

    stashing

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,3 +7,6 @@ export const SECRET_KEY = process.env.SECRET_KEY;
 export const uri = process.env.MONGO_URI;
 export const TINY_API_KEY = process.env.TINY_API_KEY;
 export const apiUrl = `https://api.tinyurl.com/create?api_token=${TINY_API_KEY}`;
+
+
+//secrets
