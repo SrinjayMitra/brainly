178bd1e15885bfb13aeeefd818ce0491d27ac696
SrinjayMitra <srinjaymitra918@gmail.com>
2025-01-12 13:51:23 -0800

    chat

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,14 +1,14 @@
-// export let URL = "https://brainly-alpha.vercel.app/api/v1";
+export let URL = "https://brainly-alpha.vercel.app/api/v1";
 
 //  local
-export let URL = "http://localhost:3000/api/v1";
+// export let URL = "http://localhost:3000/api/v1";
 
 export const SECRET_KEY = process.env.SECRET_KEY;
 export const uri = process.env.MONGO_URI;
 export const TINY_API_KEY = process.env.TINY_API_KEY;
 export const apiUrl = `https://api.tinyurl.com/create?api_token=${TINY_API_KEY}`;
 export const GEMINI_API_KEY = "AIzaSyD11h0GeflTWBWy-ca5HK83UuLM_nSrMCY";
-export const ANTHROPIC_API_KEY='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'
+// export const ANTHROPIC_API_KEY='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'
 
 
 
