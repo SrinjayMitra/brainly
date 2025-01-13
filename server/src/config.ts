46d8eaa7cd686e46989ec48ff21a6d14e5ce908e
SrinjayMitra <srinjaymitra918@gmail.com>
2025-01-12 16:38:47 -0800

    not being dumbg

--- server/src/config.ts
+++ server/src/config.ts
@@ -12,21 +12,4 @@ export const apiUrl = `https://api.tinyurl.com/create?api_token=${TINY_API_KEY}`
 
 
 
-
-
-
-
-// export const GEMINI_API_KEY = "AIzaSyD11h0GeflTWBWy-ca5HK83UuLM_nSrMCY";
-
-
-
-
-
-
-
-
-// export const TINY_API_KEY="cnCKgifMJ3y3KHycpstvL7YMGxs9QYzPmC06gq0CZoQZBfwb8APULGuGKCVI"
-// export const uri="mongodb+srv://srinjay:Srinjay%402004@cluster0.1shl9.mongodb.net/brainly"
-// export const SECRET_KEY="SEXXXXXXXXXXXXX"
-// export const apiUrl = `https://api.tinyurl.com/create?api_token=${TINY_API_KEY}`;
 //secrets
