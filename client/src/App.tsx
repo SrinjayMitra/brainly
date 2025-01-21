c8002c3408b37f1f767f0baa9f693234cb4c6863
SrinjayMitra <srinjaymitra918@gmail.com>
2025-01-21 15:04:02 -0800

    analystics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import { OnlyVideos } from './lib/OnlyVideos';
 import { SharePage } from './lib/sharePage';
 import LoaderPage from './components/ui/loader';
 import ChatApp from './lib/brainly_bot';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -57,6 +58,7 @@ const LoaderAndRoutes = () => {
         <Route path="/share/:shareLink" element={< SharePage />} />
         <Route path="/chat" element={< ChatApp />} />
       </Routes>
+      <Analytics />
     </>
   );
 }
